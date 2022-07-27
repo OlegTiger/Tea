0adfe65b9645ade576c5595a77e5bf5dad6500af
Oleg <olegsparrow3@mail.ru>
2022-07-27 19:51:58 +0300

    db+seed

---
+++ src/db/models/tea.js
@@ -0,0 +1,29 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Tea extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Comment }) {
+      // define association here
+      this.belongsTo(Comment, { foreignKey: 'tea_id' });
+    }
+  }
+  Tea.init({
+    tea_name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    place: DataTypes.STRING,
+    img: DataTypes.TEXT,
+    latitude: DataTypes.STRING,
+    longitude: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'Tea',
+  });
+  return Tea;
+};
---
+++ src/db/seeders/20220727161526-Comment.js
@@ -0,0 +1,30 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Comments', [{
+      body: 'Milk Ooholong is the best tea in the world ^_^',
+      user_id: 1,
+      tea_id: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Comments', null, {});
+  },
+};

c9ed587df5ad03092b839ab1302ab7dd466a41ef
Almas <Almaz.sgv1@mail.ru>
2022-07-28 13:43:55 +0300

    add Home components

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './Header';
+import Map from './Map';
+import Footer from './Footer';
+
+export default function Home() {
+    return (
+        <>
+
+            <Header />
+            <Map />
+            <Footer />
+        </>
+    );
+}
